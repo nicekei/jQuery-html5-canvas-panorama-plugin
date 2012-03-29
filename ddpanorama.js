568eb190205a261ce49d52ada03fcdc8d924dcb9
Kei Kim <nicekei@gmail.com>
2012-03-30 07:20:28 +0900

    * added ddpanorama_redraw handler\n*fixed redraw bug when moving mouse

--- ddpanorama.js
+++ ddpanorama.js
@@ -14,11 +14,11 @@
 //                                           );
  ddpanoramas.timerId=window.setInterval(function()
                                      {
-                                     for (var i=0; i < ddpanoramas.animations.length; ++i)
-                                     {
-                                      ddpanoramas.animations[i].update();
-                                     }
-                                     
+                                         for (var i=0; i < ddpanoramas.animations.length; ++i)
+                                         {
+                                            ddpanoramas.animations[i].update();
+                                         }
+                                         
                                      }, 1000/30);
  
  ddpanoramas.mousedown=function(x,y)
@@ -27,21 +27,22 @@
      ddpanoramas.cursorYOld=ddpanoramas.cursorY=y;
      ddpanoramas.speedX=0;
  }
+ 
  ddpanoramas.mousemove=function(x,y)
  {
- ddpanoramas.cursorXOld=ddpanoramas.cursorX;
- ddpanoramas.cursorYOld=ddpanoramas.cursorY;
- ddpanoramas.cursorX=x;
- ddpanoramas.cursorY=y;
- var currentTime=(new Date()).getTime();
- var dt=(currentTime-ddpanoramas.currentTime)/1000;
- ddpanoramas.currentTime = currentTime;
- ddpanoramas.speedX = (ddpanoramas.cursorX-ddpanoramas.cursorXOld)/dt;
- if (ddpanoramas.speedX > ddpanoramas.max_speed)
-    ddpanoramas.speedX = ddpanoramas.max_speed
- if (ddpanoramas.speedX < - ddpanoramas.max_speed)
-    ddpanoramas.speedX = -ddpanoramas.max_speed
- 
+     ddpanoramas.cursorXOld=ddpanoramas.cursorX;
+     ddpanoramas.cursorYOld=ddpanoramas.cursorY;
+     ddpanoramas.cursorX=x;
+     ddpanoramas.cursorY=y;
+     var currentTime=(new Date()).getTime();
+     var dt=(currentTime-ddpanoramas.currentTime)/1000;
+     ddpanoramas.currentTime = currentTime;
+     ddpanoramas.speedX = (ddpanoramas.cursorX-ddpanoramas.cursorXOld)/dt;
+     if (ddpanoramas.speedX > ddpanoramas.max_speed)
+        ddpanoramas.speedX = ddpanoramas.max_speed
+     if (ddpanoramas.speedX < - ddpanoramas.max_speed)
+        ddpanoramas.speedX = -ddpanoramas.max_speed
+     
  }
  
  $(document).bind("mousemove", function(event){
@@ -65,129 +66,119 @@
  $.fn.ddpanorama = function(params) 
  {//ddpanorama
  return this.each(function() {
- if ($(this).data("ddpanorama")!=null) return;
- var img=$(this);//document.createElement("img");
- var parent=$(this).parent();
- var canvas=document.createElement("canvas");
- var o={img:img,canvas:canvas,mousedown:false};
- o.params=params;
- o.remove=function()
- {
-     var index=ddpanoramas.animations.indexOf(this);
-     if (index >= 0)
-     {
-        ddpanoramas.animations.splice(index, 1);
-        //console.log("removed");
+     if ($(this).data("ddpanorama")!=null) return;
+     var img=$(this);//document.createElement("img");
+     var parent=$(this).parent();
+     var canvas=document.createElement("canvas");
+     var o={img:img,canvas:canvas,mousedown:false};
+     o.params=params;
+     o.remove=function() {
+         var index=ddpanoramas.animations.indexOf(this);
+         if (index >= 0)
+         {
+            ddpanoramas.animations.splice(index, 1);
+         }
      }
- }
- o.scrollTo=function(pageX)
- {
 
-  var mousedownScrollX=$(this.canvas).prop("mousedownScrollX");
-  if (mousedownScrollX==null) return;
-  var width=$(img).get()[0].naturalWidth;
-  var scrollX=mousedownScrollX+pageX-$(this.canvas).prop("mousedownPageX");
-  while (scrollX >= width)
-      scrollX -= width;
-  while (scrollX < 0)
-      scrollX += width;
-  $(this.canvas).prop("scrollX", scrollX);
- }
- o.update=function()
- {
+     o.scrollTo=function(pageX){
 
-     var currentTime=(new Date()).getTime();
-     var oldTime=$(this.canvas).prop("mousemoveTime");
-     $(this.canvas).prop("mousemoveTime", currentTime);
-     var dt=(currentTime-oldTime)/1000;
-     var scrollX=$(this.canvas).prop("scrollX", scrollX);
-     var speedX=$(this.canvas).prop("speedX");
-     var ctx=this.canvas.getContext('2d');
-     var width=$(this.img).get()[0].naturalWidth;//width();
-                  
-     scrollX += dt * speedX;
-     while (scrollX >= width)
-         scrollX -= width;
-     while (scrollX < 0)
-         scrollX += width;
-     //linear drag
-     speedX += - ddpanoramas.drag_constant * speedX * dt;
-     $(this.canvas).prop("speedX",speedX);
-     $(this.canvas).prop("scrollX",scrollX);
-     this.redraw();
-     if (Math.abs(speedX) < 1)
-     {
-         this.stop();
+      var mousedownScrollX=$(this.canvas).prop("mousedownScrollX");
+      if (mousedownScrollX==null) return;
+      var width=$(img).get()[0].naturalWidth;
+      var scrollX=mousedownScrollX+pageX-$(this.canvas).prop("mousedownPageX");
+      while (scrollX >= width)
+          scrollX -= width;
+      while (scrollX < 0)
+          scrollX += width;
+      $(this.canvas).prop("scrollX", scrollX);
+     }
+   
+     o.update=function(){
+         var currentTime=(new Date()).getTime();
+         var oldTime=$(this.canvas).prop("mousemoveTime");
+         $(this.canvas).prop("mousemoveTime", currentTime);
+         var dt=(currentTime-oldTime)/1000;
+         var scrollX=$(this.canvas).prop("scrollX", scrollX);
+         var speedX=$(this.canvas).prop("speedX");
+         var ctx=this.canvas.getContext('2d');
+         var width=$(this.img).get()[0].naturalWidth;//width();
+                      
+         scrollX += dt * speedX;
+         while (scrollX >= width)
+             scrollX -= width;
+         while (scrollX < 0)
+             scrollX += width;
+         //linear drag
+         speedX += - ddpanoramas.drag_constant * speedX * dt;
+         $(this.canvas).prop("speedX",speedX);
+         $(this.canvas).prop("scrollX",scrollX);
+         this.redraw();
+         if (Math.abs(speedX) < 1)
+         {
+             this.stop();
+             this.remove();
+         }
+     }
+     o.stop=function(){
+        $(this.canvas).prop("speedX", 0);
+        //console.log("o.stop");
+     }
+ 
+     o.redraw = function(){
+         var width=$(this.img).get()[0].naturalWidth;//width();
+         var ctx=this.canvas.getContext('2d');
+         var scrollX=$(this.canvas).prop("scrollX");
+         
+         var imgDOM=this.img.get()[0];
+         ctx.drawImage(imgDOM, scrollX,0);
+         ctx.drawImage(imgDOM, scrollX+width,0);
+         ctx.drawImage(imgDOM, scrollX-width,0);
+         $(this.img).trigger(jQuery.Event("ddpanorama_redraw", {canvas:this.canvas,speed:$(this.canvas).prop("speedX")/ddpanoramas.max_speed}));
+     }
+
+     o.onmousedown=function(pageX){
+         //console.log("mousedown");
+         var o= this;
+         var canvas=this.canvas;
+         this.mousedown=true;
+         $(canvas).prop("speedX",0);
+         var currentTime=(new Date()).getTime();
          this.remove();
+         this.stop();
+         $(canvas).prop("mousedownPageX", pageX);
+         $(canvas).prop("mousedownScrollX", $(canvas).prop("scrollX"));
+         $(canvas).prop("mousemoveTime", currentTime);
      }
- }
-  o.stop=function()
-  {
-    $(this.canvas).prop("speedX", 0);
-    //console.log("o.stop");
-  }
- 
- o.redraw = function()
- {
-//     var width=this.img.width();
-                  var width=$(this.img).get()[0].naturalWidth;//width();
-                  
-     var ctx=this.canvas.getContext('2d');
-     var scrollX=$(this.canvas).prop("scrollX");
-     
-     //console.log("img.get()");
-     var imgDOM=this.img.get()[0];
-     //console.log(img.get());
-     ctx.drawImage(imgDOM, scrollX,0);
-     ctx.drawImage(imgDOM, scrollX+width,0);
-     ctx.drawImage(imgDOM, scrollX-width,0);
- 
- }
-  o.onmousedown=function(pageX)
-  {
-      //console.log("mousedown");
-      var o= this;
-      var canvas=this.canvas;
-      this.mousedown=true;
-      $(canvas).prop("speedX",0);
-      var currentTime=(new Date()).getTime();
-      this.remove();
-      this.stop();
-      $(canvas).prop("mousedownPageX", pageX);
-      $(canvas).prop("mousedownScrollX", $(canvas).prop("scrollX"));
-      $(canvas).prop("mousemoveTime", currentTime);
-  }
-  o.resize=function()
-  {
-      var img=this.img;
-      var canvas=this.canvas;
-      var width=img.get()[0].naturalWidth;//width();
-      var height=img.get()[0].naturalHeight; //height();
-      if (o.params.hasOwnProperty("ratio"))
-      {
-          width=height/o.params.ratio;
-      }
-      else if (o.params.hasOwnProperty("width"))
-      {
-          width=params.width;
-      }
-      //console.log('width:'+width);
-      $(canvas).attr('width', width);
-      $(canvas).attr('height', height);
-  
-  }
 
-  $(this).data('ddpanorama', o);
-  $(canvas).data('ddpanorama', o);
-  $(canvas).prop("scrollX",0);
-  $(canvas).prop("speedX",0);
-  $(canvas).mousedown(function(event)
-                      {
-                      var o= $(this).data('ddpanorama');
-                      o.onmousedown(event.pageX);
+     o.resize=function(){
+         var img=this.img;
+         var canvas=this.canvas;
+         var width=img.get()[0].naturalWidth;//width();
+         var height=img.get()[0].naturalHeight; //height();
+         if (o.params.hasOwnProperty("ratio"))
+         {
+             width=height/o.params.ratio;
+         }
+         else if (o.params.hasOwnProperty("width"))
+         {
+             width=params.width;
+         }
+         //console.log('width:'+width);
+         $(canvas).attr('width', width);
+         $(canvas).attr('height', height);
+     
+     }
+
+    $(this).data('ddpanorama', o);
+    $(canvas).data('ddpanorama', o);
+    $(canvas).prop("scrollX",0);
+    $(canvas).prop("speedX",0);
+    $(canvas).mousedown(function(event){
+                          var o= $(this).data('ddpanorama');
+                          o.onmousedown(event.pageX);
                       });
-  var mouseup=function(event)
-  {
+
+    var mouseup=function(event){
       var o= $(this).data('ddpanorama');
       if (o.mousedown==false) return;
       o.mousedown=false;
@@ -199,29 +190,26 @@
       $(canvas).prop("speedX", ddpanoramas.speedX );
       $(canvas).prop("mousedownScrollX", null);
       //console.log("mouseup:speedX:"+speedX);
-  };
-  $(canvas).bind("mouseup",mouseup)
-  $(canvas).bind("mouseupoutside",mouseup);
+    };
+    $(canvas).bind("mouseup",mouseup)
+    $(canvas).bind("mouseupoutside",mouseup);
                   
-  $(canvas).bind("mousemove", function(event){
-                 
+    $(canvas).bind("mousemove", function(event){
                           var pageX=event.pageX;
                           var o= $(this).data('ddpanorama');
+                          if (o.mousedown==false) return;
                           o.scrollTo(pageX);
                           o.redraw();
                       });
-  $(canvas).bind("contextmenu", function(event){
-                 
+    $(canvas).bind("contextmenu", function(event){
                  e.preventDefault();
                  return false;                 
                  });                
-  $(canvas).bind("touchstart", function(event)
-                 {
+    $(canvas).bind("touchstart", function(event) {
                  var o= $(this).data('ddpanorama');
                  o.onmousedown(event.originalEvent.touches[0].pageX);
                  });
-  $(canvas).bind("touchmove", function(event){
-                 
+    $(canvas).bind("touchmove", function(event){
                  //document.write();
                  //console.log("touchmove");
                  event.preventDefault();
@@ -231,13 +219,12 @@
                  o.scrollTo(pageX);
                  o.redraw();
                  });
-  $(canvas).bind("touchend",mouseup);
-  o.resize();
-  o.redraw();
-  $(this).css('display','none');
-  parent.append(canvas);
-  $(this).load(function ()
-             {
+    $(canvas).bind("touchend",mouseup);
+    o.resize();
+    o.redraw();
+    $(this).css('display','none');
+    parent.append(canvas);
+    $(this).load(function (){
                o.resize();
                o.redraw();
              });//load
